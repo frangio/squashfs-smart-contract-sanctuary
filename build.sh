33132b91c0e5dd6e43739431226b441c1ea5bd80
Francisco Giordano <frangio.1@gmail.com>
2022-08-03 15:34:48 -0300

    use token

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 chains=(arbitrum avalanche bsc celo ethereum fantom optimism polygon tron)
 
 for c in chains; do
-  git clone --depth=1 https://github.com/tintinweb/smart-contract-sanctuary-$c.git $c
+  git clone --depth=1 https://x-access-token:$GITHUB_TOKEN@github.com/tintinweb/smart-contract-sanctuary-$c.git $c
   mksquashfs $c smart-contract-sanctuary.sqsh -comp lzo -keep-as-directory -wildcards -e '... .git'
   rm -rf $c
 done
