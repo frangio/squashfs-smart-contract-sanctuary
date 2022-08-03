8be7c9f66c7c6a49d784d1bccd59a27989fbe94b
Francisco Giordano <frangio.1@gmail.com>
2022-08-03 18:59:58 -0300

    add progress bar

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ chains=(arbitrum avalanche bsc celo ethereum fantom optimism polygon tron)
 
 for c in ${chains[@]}; do
   git clone --depth=1 https://x-access-token:$GITHUB_TOKEN@github.com/tintinweb/smart-contract-sanctuary-$c.git $c
-  mksquashfs $c smart-contract-sanctuary.sqsh -comp lzo -keep-as-directory -wildcards -e '... .git'
+  mksquashfs $c smart-contract-sanctuary.sqsh -comp lzo -progress -keep-as-directory -wildcards -e '... .git'
   rm -rf $c
 done
