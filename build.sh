ac6ead1d6dac700d21df7678100d74dc12d3aabd
Francisco Giordano <frangio.1@gmail.com>
2022-08-03 11:08:52 -0300

    use shell script

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euo
+
+chains=(arbitrum avalanche bsc celo ethereum fantom optimism polygon tron)
+
+for c in chains; do
+  git clone --depth=1 git@github.com:tintinweb/smart-contract-sanctuary-$c.git $c
+  mksquashfs $c smart-contract-sanctuary.sqsh -comp lzo -keep-as-directory -wildcards -e '... .git'
+  rm -rf $c
+done
